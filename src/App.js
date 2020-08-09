011a51d15f693c79f59b81b0077b4d3246e59713
Tyler Coil <kamori@MacBook-Pro.grandenetworks.net>
2020-08-09 18:04:22 -0500

    Initializing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Reddit subreddit="/r/programming/" />
+    </div>
+  )
+
+}
+
+function Reddit(props) {
+  const [posts, setPosts] = useState([]);
+  const [subredditList, setSubredditList] = useState([]);
+  const [subreddit, setSubreddit] = useState(props.subreddit);
+
+  const getPosts = (subr) => {
+    setPosts([]);
+    axios.get(`https://www.reddit.com${subr ? subr.slice(0, -1) : subreddit.slice(0, -1)}.json`)
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+
+        setPosts(newPosts);
+      });
+  };
+
+
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/subreddits.json`)
+      .then(res => {
+        const subReddits = res.data.data.children
+          .map(obj => obj.data.url);
+
+        setSubredditList(subReddits);
+        getPosts();
+      });
+  }, []);
+
+
+
+
+  return (
+    <div>
+      <h1>{subreddit}</h1>
+
+      <select onChange={(val) => { console.log(val.target.value); setSubreddit(val.target.value); getPosts(val.target.value); }}>
+        <option value={props.subreddit}>{props.subreddit}</option>
+        {subredditList.map(sub => (
+          <option value={sub}>{sub}</option>
+        ))}
+      </select>
+
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            {post.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
